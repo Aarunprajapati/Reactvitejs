4812eb06c0c6fd8e6d285dbed8991c8f9acf4d43
Arun <arunprajapati8502@gmail.com>
2023-10-24 21:03:49 +0530

    Todocontext completed

---
+++ todocontextapp/src/components/index.js
@@ -0,0 +1,5 @@
+import TodoForm from "./TodoForm";
+import Todoitem from "./TodoForm";
+
+
+export {TodoForm,Todoitem}
---
+++ todocontextapp/src/context/index.js
@@ -0,0 +1,5 @@
+import { TodoContext } from "./todocontext"
+import { TodoProvider } from "./todocontext"
+import { useTodo } from "./todocontext"
+
+export {TodoProvider,TodoContext,useTodo}
